1a85762acaa81102f750d582545d3660dfa08640
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-05-22 15:54:34 +0900

    [2]2페이지 90퍼완료

--- app/src/main/java/com/example/recipebookapp/MainActivity2.java
+++ app/src/main/java/com/example/recipebookapp/MainActivity2.java
@@ -24,8 +24,11 @@ public class MainActivity2 extends AppCompatActivity {
         itemModels = new ArrayList<>();
         // 아이템 데이터를 생성하여 itemModels에 추가합니다.
         itemModels.add(new ItemModel("김치 볶음밥", "10 Min", R.drawable.food1, 4.5f));
-        itemModels.add(new ItemModel("제육 볶음", "15 Min", R.drawable.food2, 3.8f));
-        itemModels.add(new ItemModel("크림 리조또", "20 Min", R.drawable.food2, 4.2f));
+        itemModels.add(new ItemModel("닭 볶음탕", "30 Min", R.drawable.food2, 3.8f));
+        itemModels.add(new ItemModel("제육 볶음", "20 Min", R.drawable.food3, 4.2f));
+        itemModels.add(new ItemModel("크림 리조또", "15 Min", R.drawable.food4, 3.8f));
+        itemModels.add(new ItemModel("고구마 맛탕", "20 Min", R.drawable.food5, 3.8f));
+        itemModels.add(new ItemModel("오뎅탕", "10 Min", R.drawable.food6, 3.8f));
         // 추가적인 아이템 데이터를 추가할 수 있습니다.
 
         itemAdapter = new ItemAdapter(this, itemModels);
