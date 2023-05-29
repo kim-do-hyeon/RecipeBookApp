b594bf31fe5555434cbe7118896bec2f10278eb2
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-05-30 02:11:59 +0900

    [7] 레시피 페이지에 아이템데이터 적용

--- app/src/main/java/com/example/recipebookapp/RecipeActivity.java
+++ app/src/main/java/com/example/recipebookapp/RecipeActivity.java
@@ -10,11 +10,17 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.recipebookapp.R;
 
 public class RecipeActivity extends AppCompatActivity {
     private FrameLayout modalContainer;
+    private TextView foodTitle;
+    private TextView recipeTime;
+
+    private ImageView recipeImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
