6a6631604de89bc936a04ed2409172e7019b4232
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-05-30 01:52:36 +0900

    [6]필터버튼 모달창 적용

--- app/src/main/java/com/example/recipebookapp/MainActivity2.java
+++ app/src/main/java/com/example/recipebookapp/MainActivity2.java
@@ -1,8 +1,20 @@
 package com.example.recipebookapp;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +25,11 @@ public class MainActivity2 extends AppCompatActivity {
     private ItemAdapter itemAdapter;
     private ArrayList<ItemModel> itemModels;
 
+    private AppCompatImageButton filterButton;
+
+    private Dialog filterDialog;
+
+    @SuppressLint({"ClickableViewAccessibility", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +41,37 @@ public class MainActivity2 extends AppCompatActivity {
         itemModels = new ArrayList<>();
         // 아이템 데이터를 생성하여 itemModels에 추가합니다.
         itemModels.add(new ItemModel("김치 볶음밥", "10 Min", R.drawable.food1, 4.5f));
-<<<<<<< Updated upstream
         itemModels.add(new ItemModel("닭 볶음탕", "30 Min", R.drawable.food2, 3.8f));
         itemModels.add(new ItemModel("제육 볶음", "20 Min", R.drawable.food3, 4.2f));
         itemModels.add(new ItemModel("크림 리조또", "15 Min", R.drawable.food4, 3.8f));
         itemModels.add(new ItemModel("고구마 맛탕", "20 Min", R.drawable.food5, 3.8f));
         itemModels.add(new ItemModel("오뎅탕", "10 Min", R.drawable.food6, 3.8f));
-=======
-        itemModels.add(new ItemModel("제육 볶음", "15 Min", R.drawable.food2, 3.0f));
-        itemModels.add(new ItemModel("크림 리조또", "20 Min", R.drawable.food2, 2.2f));
->>>>>>> Stashed changes
         // 추가적인 아이템 데이터를 추가할 수 있습니다.
 
         itemAdapter = new ItemAdapter(this, itemModels);
         recyclerView.setAdapter(itemAdapter);
+
+        filterButton = findViewById(R.id.filter_Button);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // filter.xml을 인플레이션하여 다이얼로그로 표시
+                LayoutInflater inflater = LayoutInflater.from(MainActivity2.this);
+                View filterView = inflater.inflate(R.layout.filter, null);
+
+                filterDialog = new Dialog(MainActivity2.this);
+                filterDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                filterDialog.setContentView(filterView);
+
+                // 다이얼로그 크기 및 위치 설정
+                Window window = filterDialog.getWindow();
+                WindowManager.LayoutParams layoutParams = window.getAttributes();
+                layoutParams.gravity = Gravity.TOP;
+                layoutParams.y = 300;
+                window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 900);
+
+                filterDialog.show();
+            }
+        });
     }
 }
